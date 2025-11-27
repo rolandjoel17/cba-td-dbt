7e3aacfabe57db4a4639144e5224bb2a7285660c
rolandjoel17 <roland.joel@gmail.com>
2025-11-27 17:25:30 +1100

    Add dbt models: customer_summary
    Created via CBA Migration Assistant
    Timestamp: 2025-11-26T22:25:30.126246

--- models/public/customer_summary.sql
+++ models/public/customer_summary.sql
@@ -8,7 +8,7 @@
 }}
 
 -- Source: CBA Migration Assistant
--- Created: 2025-11-26 22:13:18
+-- Created: 2025-11-26 22:25:06
 -- Target: CBA_DEMO.PUBLIC.CUSTOMER_SUMMARY
 
-SELECT customer_id, COUNT(*) as total_count FROM customers GROUP BY customer_id
+SELECT customer_id, COUNT(*) as total_count FROM CBA_DEMO.PUBLIC.CUSTOMER_DATA GROUP BY customer_id
