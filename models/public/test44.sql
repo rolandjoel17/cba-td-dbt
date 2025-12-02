5411af795f7d7d01545c7465e54d2de855ac34fb
rolandjoel17 <roland.joel@gmail.com>
2025-12-02 17:15:08 +1100

    Add dbt model: test44 - Customer transaction count analysis

---
+++ models/public/test44.sql
@@ -0,0 +1,14 @@
+{{
+  config(
+    materialized='dynamic_table',
+    target_lag='1 hour',
+    snowflake_warehouse='DEMO_WH',
+    on_schema_change='fail'
+  )
+}}
+
+-- Source: CBA Migration Assistant
+-- Created: 2025-12-01 22:14:53
+-- Target: CBA_DEMO.PUBLIC.test44
+
+SELECT customer_id, COUNT(*) as total_count FROM CBA_DEMO.PUBLIC.CUSTOMER_DATA GROUP BY customer_id
